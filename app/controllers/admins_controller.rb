a600bcb3d1be43b37634f79798a2f25de7608998
cloverharvest <anna@schwabdev.com>
2016-05-26 16:06:55 -0700

    Build methods in admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,14 @@
 class AdminsController < ApplicationController
   def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params[:user])
+    if @admin.save
+      redirect_to home_path, :notice => "Signed up!"
+    else
+      render :new
+    end
   end
 end
