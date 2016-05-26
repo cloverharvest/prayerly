9043a0367d4fbbfe0a71bb569a7920929c63b8fb
cloverharvest <anna@schwabdev.com>
2016-05-26 16:29:37 -0700

    Added methods to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    admin = Admin.authenticate(params[:email], params[:password])
+    if admin
+      session[:admin_id] = admin.id
+      redirect_to home_path, :notice =>"Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    redirect_to home_path, :notice => "Logged out!"  
+  end
 end
