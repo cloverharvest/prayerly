4bee9aec5b39d9f7089f8541b4c56df6c481fcef
cloverharvest <anna@schwabdev.com>
2016-05-27 09:58:20 -0700

    Redirect new admin to admin index page for now

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -9,7 +9,7 @@ class AdminsController < ApplicationController
   def create
     @admin = Admin.new(admin_params)
     if @admin.save
-      redirect_to root_path, :notice => "Signed up!" #going to root_path for now, will be redirected to admins_index or prayers#index
+      redirect_to admins_path, :notice => "Signed up!" #going to root_path for now, will be redirected to admins_index or prayers#index
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "log_out", to: "sessions#destroy", :as => "log_out"
   get "log_in", to: "sessions#new", :as => "log_in"
-  get "sign_up", to: "admins#new", :as => "sign_up"
+  get "sign_up", to: "admins#new", :as => "new_admin"
 
   root :to => "home#index"
 
