66e894ef66f752938f3680687a94c4a251124662
cloverharvest <anna@schwabdev.com>
2016-05-29 11:28:57 -0700

    Edited models to include associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :categoryprayers, dependent: :destroy
+  has_many :prayers, through: :categoryprayers
 end
--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,5 +1,7 @@
 class Prayer < ActiveRecord::Base
-  
+  has_many :categoryprayers, dependent: :destroy
+  has_many :categories, through: :categoryprayers
+
   def self.search(search)
     where("title ILIKE ? OR text ILIKE ?", "%#{search}%", "%#{search}%",)
   end
