2fb5285411eb12e284fbfead6418f7cf59eddc87
cloverharvest <anna@schwabdev.com>
2016-05-28 17:51:08 -0700

    Refine navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     admin = Admin.authenticate(params[:email], params[:password])
     if admin
       session[:admin_id] = admin.id
-      redirect_to root_path, :notice =>"Logged in!"
+      redirect_to prayers_path, :notice =>"Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render :new
