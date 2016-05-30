194cd77a59f77b233b4bc54bc2ec9944e6c4279c
cloverharvest <anna@schwabdev.com>
2016-05-29 23:32:01 -0700

    Tests navbar color, restricts enduser access, puts routing for categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   get "log_in", to: "sessions#new", as: "log_in"
   get "sign_up", to: "admins#new", as: "sign_up"
 
-  root :to => "home#index"
+  root :to => "categories#index"
 
   resources :admins
   resources :sessions
   resources :prayers
+  resources :categories
 
-  delete "prayers", to: "prayers#destroy" 
+  delete "prayers", to: "prayers#destroy"
+
+  get "categories", to: "categories#index"
 end
