e43a1d2a6b0c452bd4696702dd911ddb5f6b715c
cloverharvest <anna@schwabdev.com>
2016-05-30 14:51:31 -0700

    Added categories controller nd changed root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
   get "log_out", to: "sessions#destroy", as: "log_out"
   get "log_in", to: "sessions#new", as: "log_in"
   get "sign_up", to: "admins#new", as: "sign_up"
 
-  root :to => "categories#index"
+  root :to => "home#index"
 
   resources :admins
   resources :sessions
