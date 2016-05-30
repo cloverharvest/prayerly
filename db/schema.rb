e317170d4b24fdff0c1099df5fced48e2173c655
cloverharvest <anna@schwabdev.com>
2016-05-30 15:12:53 -0700

    Created comment model and attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530001819) do
+ActiveRecord::Schema.define(version: 20160530221022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20160530001819) do
     t.string   "cat_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "prayer_intention"
+    t.integer  "prayer_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "prayers", force: :cascade do |t|
