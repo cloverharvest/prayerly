2f8d797a0db54d5e255ec1a63fcd70387076022f
cloverharvest <anna@schwabdev.com>
2016-05-30 18:07:21 -0700

    Made views for new comments in particular prayer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,23 @@
 class CommentsController < ApplicationController
   def index
-  end
-
-  def create
+    @comments = Comment.all
+    render :index
   end
 
   def new
+    @comment = Comment.new
+
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:success] = "New comment added."
+      redirect_to prayer_comment_path(@prayer)
+    else
+      flash[:error] = @comment.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -13,4 +25,10 @@ class CommentsController < ApplicationController
 
   def show
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:name, :prayer_intention, :prayer_id)
+    end
 end
