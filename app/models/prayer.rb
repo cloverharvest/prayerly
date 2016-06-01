f55d0613f5c1fe530e0167497d255d42118a45f3
cloverharvest <anna@schwabdev.com>
2016-06-01 15:39:08 -0700

    Adds validation for prayer model

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -4,6 +4,10 @@ class Prayer < ActiveRecord::Base
 
   has_many :comments
 
+  validates :image, :title, :text, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :text, length: { in: 5..3000, too_long: "%{count} characters is the maximum allowed"}
+
   def self.search(search)
     where("title ILIKE ? OR text ILIKE ?", "%#{search}%", "%#{search}%",)
   end
