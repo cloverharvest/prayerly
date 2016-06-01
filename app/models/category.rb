9adf60c18ab20682925521fe21d0282fd6533118
cloverharvest <anna@schwabdev.com>
2016-06-01 15:46:54 -0700

    Adds validation for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :specific_prayers, dependent: :destroy
   has_many :prayers, through: :specific_prayers
+
+  validates :cat_name, presence: true, length: { in: 5..100, too_long: "%{count} characters is the maximum allowed"}
+
 end
